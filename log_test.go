3cc7cd837dbf087eac362fb295b18ce60fec3735
datewu <hihahajun@gmail.com>
2021-10-28 09:39:20 +0800

    update to go 1.17.2; add level test

---
+++ log_test.go
@@ -0,0 +1,24 @@
+package jsonlog
+
+import (
+	"bytes"
+	"errors"
+	"strings"
+	"testing"
+)
+
+func TestLevel(t *testing.T) {
+	var buf bytes.Buffer
+	l := New(&buf, LevelError)
+	l.PrintInfo("info", nil)
+	if buf.Len() != 0 {
+		t.Errorf("Expected no output, got %q", buf.String())
+		t.Failed()
+	}
+	err := errors.New("error lol")
+	l.PrintErr(err, nil)
+	if strings.Contains(buf.String(), "error lol\n") {
+		t.Errorf("Expected error output, got %q", buf.String())
+		t.Failed()
+	}
+}
